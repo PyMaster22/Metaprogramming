5f97dd7b55e6dd53cd9cb185f7e79d6c96d0902c
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2026-08-24 00:52:06 -0700

    Turing.cpp Now Tested and Stepper.cpp Compatible

--- Turing.cpp
+++ Turing.cpp
@@ -1,5 +1,9 @@
 #ifndef TEMPLATE_TURINGMACHINE_
 #define TEMPLATE_TURINGMACHINE_
+// Anon namespacing helpers doesn't actually work.
+// This is required to prevent collisions.
+// I.e. use random characters to create a unique-ish namespace.
+#define Helpers_ S4YM0SUJ4VJQ5BWRD6CT1JCH0XPS2AGM
 
 template<class... Symbols> struct Tape;
 struct HaltState{}; // Definition required for specialization
@@ -7,12 +11,39 @@ struct Left{};
 struct Right{};
 struct Blank{}; // For when Tape is overrun in either direction
 
-template<
-    class TapeLeft, class TapeCurrent, class TapeRight, 
-    class State, template<class QState, class QSymbol> class StateDeltas
-> struct MachineConfiguration;
-// MachineStepper should not be used elsewhere. Hence, you may not.
-namespace{
+namespace Helpers_{
+	template<class Tape1, class Tape2> struct MergeTapes;
+	template<class Tape> struct ReverseTape;
+	template<class TapeLeft, class Current, class TapeRight> struct ReadOut;
+
+	template<class Symbol1, class... Symbols>
+	struct ReverseTape<Tape<Symbol1,Symbols...>>{
+		typedef typename MergeTapes<
+			typename ReverseTape<Tape<Symbols...>>::value,
+			Tape<Symbol1>
+		>::value value;
+	};
+	template<> struct ReverseTape<Tape<>>{
+		typedef Tape<> value;
+	};
+
+	template<class... Symbols1, class... Symbols2>
+	struct MergeTapes<Tape<Symbols1...>,Tape<Symbols2...>>{
+		typedef Tape<Symbols1...,Symbols2...> value;
+	};
+
+	template<class Left, class CurSymbol, class Right>
+	struct ReadOut{
+		typedef typename MergeTapes<
+			typename MergeTapes<
+				typename ReverseTape<Left>::value,
+				Tape<CurSymbol>
+			>::value,
+			Right
+		>::value value;
+	};
+
+	
 	template<
 	    class TapeLeft, class TapeRight,
 	    class NewState, class NewSymbol, class Direction,
@@ -23,12 +54,18 @@ namespace{
 template<
     class TapeLeft, class TapeCurrent, class TapeRight, 
     class State, template<class QState, class QSymbol> class StateDeltas
+> struct MachineConfiguration;
+
+template<
+    class TapeLeft, class TapeCurrent, class TapeRight, 
+    class State, template<class QState, class QSymbol> class StateDeltas
 > struct MachineConfiguration{
+	typedef typename Helpers_::ReadOut<TapeLeft,TapeCurrent,TapeRight>::value Out;
     typedef StateDeltas<State,TapeCurrent> delta;
     typedef typename delta::NewState newState;
     typedef typename delta::NewSymbol newSymbol;
     typedef typename delta::Direction direction;
-    typedef typename MachineStepper<
+    typedef typename Helpers_::MachineStepper<
         TapeLeft,TapeRight,
         newState,newSymbol,direction,
         StateDeltas
@@ -39,14 +76,15 @@ template<
     class TapeLeft, class TapeCurrent, class TapeRight, 
     template<class QState, class QSymbol> class StateDeltas
 > struct MachineConfiguration<TapeLeft,TapeCurrent,TapeRight,HaltState,StateDeltas>{
+	typedef typename Helpers_::ReadOut<TapeLeft,TapeCurrent,TapeRight>::value Out;
     typedef MachineConfiguration<
         TapeLeft,TapeCurrent,TapeRight,
         HaltState,StateDeltas
     > Next;
 };
 
-// ditto
-namespace{
+
+namespace Helpers_{
 	/* Write + move Left */
 	template<
 	    class LeftSymbol1, class... LeftSymbols, class... RightSymbols,
@@ -101,4 +139,5 @@ namespace{
 	};
 }
 
+#undef Helpers_
 #endif
