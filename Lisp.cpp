f01ff65c4f1c2a555ca11b8ba8fdf9c9d215c477
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2026-09-09 03:31:46 -0700

    Re-cons-ify

--- Lisp.cpp
+++ Lisp.cpp
@@ -11,14 +11,14 @@ template<class car, class cdr> struct cons{
 };
 template<class Name> struct Symbol;
 
-template<class Expression, class TrueCase, class FalseCase> struct If;
-template<class Expression> struct Quote;
-template<class Expression> struct Atom;
-template<class Expression> struct Car;
-template<class Expression> struct Cdr;
+struct If;
+struct Quote;
+struct Atom;
+struct Car;
+struct Cdr;
 
-template<class Argument, class Expression> struct Lambda;
-template<class Lambda, class Parameter> struct Application;
+struct Lambda;
+struct Application;
 
 // Environment is CONS!
 //template<class Name, class Value> struct Association;
@@ -82,37 +82,37 @@ struct Evaluate<Symbol<SymbolName>,Environment>{
 	typedef typename Helpers_::SymbolLookup<SymbolName,Environment>::value value;
 };
 template<class Expression, class TrueCase, class FalseCase, class Environment>
-struct Evaluate<If<Expression,TrueCase,FalseCase>,Environment>{
+struct Evaluate<cons<If,cons<Expression,cons<TrueCase,cons<FalseCase,Nil>>>>,Environment>{
 	typedef typename Ternary<typename Evaluate<Expression,Environment>::value,
 		Evaluate<TrueCase,Environment>,
 		Evaluate<FalseCase,Environment>
 	>::value::value value;
 };
 template<class Expression, class Environment>
-struct Evaluate<Quote<Expression>,Environment>{
+struct Evaluate<cons<Quote,Expression>,Environment>{
 	typedef Expression value;
 };
 template<class Expression, class Environment>
-struct Evaluate<Atom<Expression>,Environment>{
+struct Evaluate<cons<Atom,Expression>,Environment>{
 	typedef typename Helpers_::IsAtom<
 		typename Evaluate<Expression,Environment>::value
 	>::value value;
 };
 template<class Expression, class Environment>
-struct Evaluate<Car<Expression>,Environment>{
+struct Evaluate<cons<Car,Expression>,Environment>{
 	typedef typename Evaluate<Expression,Environment>::value::CAR value;
 };
 template<class Expression, class Environment>
-struct Evaluate<Cdr<Expression>,Environment>{
+struct Evaluate<cons<Cdr,Expression>,Environment>{
 	typedef typename Evaluate<Expression,Environment>::value::CDR value;
 };
 
 template<class Argument, class Expression, class Environment>
-struct Evaluate<Lambda<Argument,Expression>,Environment>{
+struct Evaluate<cons<Lambda,cons<Argument,cons<Expression,Nil>>>,Environment>{
 	typedef Closure<Argument,Expression,Environment> value;
 };
 template<class Lambda, class Parameter, class Environment>
-struct Evaluate<Application<Lambda,Parameter>,Environment>{
+struct Evaluate<cons<Application,cons<Lambda,cons<Parameter,Nil>>>,Environment>{
 	typedef typename Helpers_::Apply<
 		typename Evaluate<Lambda,Environment>::value,
 		typename Evaluate<Parameter,Environment>::value
