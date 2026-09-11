79126270b659bc58a3a34f79b047ed81eecaf600
PyMaster22 <59183571+PyMaster22@users.noreply.github.com>
2026-09-10 22:37:36 -0700

    Slight modifications with Peano for better API support

--- CPreProcessor/Peano/Peano.c
+++ CPreProcessor/Peano/Peano.c
@@ -1,13 +1,15 @@
 #ifndef MACROMAGIC_PEANOARITHMETIC_
 #define MACROMAGIC_PEANOARITHMETIC_
 #include "../Base.c"
-#include "Logic.c"
+
+// To work with Iff and Bool
+#define NotHelpers_Zero PROBE()
 
 #define AddHelpers_Succ(x) AddHelpers_SuccMover(x
 #define AddHelpers_Zero SecondArgument(NULL
 #define AddHelpers_SuccMover(x,y) DEFER(AddHelpers_Indirection)()(x,Succ(y))
 #define AddHelpers_Indirection() AddHelpers_Add2
-#define AddHelpers_Add2(x,y) AddHelpers_ ## x, y)
+#define AddHelpers_Add2(x,y) Glue(AddHelpers_,x), y)
 #define AddHelpers_Add(x,y) AddHelpers_Add2(x,y)
 
 #define Add(x,y) EVAL(AddHelpers_Add(EVAL(x),EVAL(y)))
@@ -17,7 +19,7 @@
 #define MultiplyHelpers_Zero SecondArgument(NULL
 #define MultiplyHelpers_Adder(x,y,z) DEFER(MultiplyHelpers_Indirection)()(x,Add(y,z),z)
 #define MultiplyHelpers_Indirection() MultiplyHelpers_Mult2
-#define MultiplyHelpers_Mult2(x,y,z) MultiplyHelpers_ ## x, y, z)
+#define MultiplyHelpers_Mult2(x,y,z) Glue(MultiplyHelpers_,x), y, z)
 #define MultiplyHelpers_Mult(x,y) MultiplyHelpers_Mult2(x,Zero,y)
 
 // Broken past 1*y for whatever reason
